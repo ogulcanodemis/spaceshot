646efc34315d9170cf592a070e5dc1f9fa7f8e9d
Anıl Şadioğlu <ogulcan.odemis28@gmail.com>
2021-11-11 17:46:22 +0300

    110121

--- spaceshot/Assets/scripts/PlayerControls.cs
+++ spaceshot/Assets/scripts/PlayerControls.cs
@@ -4,20 +4,48 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField] float controlSpeed= 15f;
+    [SerializeField] float controlSpeed= 15f; 
+    [SerializeField] float Xrange = 16f; // ekran sýnýr
+    [SerializeField] float Yrange = 12f; // ekran sýnýr
 
-    // Update is called once per frame
+    [SerializeField] float positionPitchFactor= -2f; // X ekseni 
+    [SerializeField] float controlPitchFactor = -3f; // 
+    [SerializeField] float positionYawFactor; //  Y ekseni
+    [SerializeField] float controlRollFactor = 20f; //  Z ekseni
+    float yThrow;
+    float xThrow;
+
     void Update()
     {
-        float xThrow = Input.GetAxis("Horizontal");
-        float YThrow = Input.GetAxis("Vertical");
+        processTransition();
+        processRotation();
+    }
+
+    void processRotation() 
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor; //yukarý aþaðý rotation Y
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+
+        float pitch = pitchDueToControlThrow  *pitchDueToPosition ;
+
+        float yaw = transform.localPosition.x * positionYawFactor; // sað sol rotation X
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw , roll); // açý berlirleme 
+    }
+
+    void processTransition()
+    {
+         xThrow = Input.GetAxis("Horizontal");
+         yThrow = Input.GetAxis("Vertical");
 
         float xOffset = xThrow * Time.deltaTime * controlSpeed;
-        float yOffset = YThrow * Time.deltaTime * controlSpeed;
+        float rawXpos = transform.localPosition.x + xOffset;
+        float clampedXpos = Mathf.Clamp(rawXpos, -Xrange, Xrange); // ekran
+
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
         float newYpos = transform.localPosition.y + yOffset;
-        float newXpos = transform.localPosition.x + xOffset;
-        transform.localPosition = new Vector3(newXpos,newYpos,transform.localPosition.z);
-        
+        float clampedYpos = Mathf.Clamp(newYpos, -Yrange, Yrange); // ekran
+
+        transform.localPosition = new Vector3(clampedXpos, clampedYpos, transform.localPosition.z); // ekrandan çýkmama
     }
 }
